1e063e60238110c4a687f5502c14ad4149be1f1e
Artem Vasenin <vasart169@gmail.com>
2025-12-07 15:14:00 +0000

    feat: Add bgg link

--- rizhiy_com/index.py
+++ rizhiy_com/index.py
@@ -11,6 +11,7 @@ def index():
         linkedin="https://linkedin.com/in/rizhiy/",
         imdb="https://www.imdb.com/user/ur62161138/ratings/",
         goodreads="https://www.goodreads.com/rizhiy",
+        bgg="https://boardgamegeek.com/collection/user/Rizhiy",
         steam="https://steamcommunity.com/id/Rizhiy/",
         mal="https://myanimelist.net/animelist/rizhiy",
     )
